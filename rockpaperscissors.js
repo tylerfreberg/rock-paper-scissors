24e27f16936f71da217521f393a8e0cd1d3cc79c
Tyler Freberg <tylerfreberg1@gmail.com>
2026-03-01 21:30:04 -0600

    Logic was backwards on rock vs scissors cases, updated to correct logic. Created an infinite loop so the game repeats without having to reload the page.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -7,73 +7,76 @@
 // Scores. Best of 3.
 let playerScore = 0
 let cpuScore = 0
-let validchoice = false;
-let playerNum = "";
+let playerNum;
+let validchoice;
 let choices = ["rock", "paper", "scissors"];
 
-let cpuNum = (Math.floor(Math.random() * 3)); // picks a random integer between 0-2. O = "Rock", 1 = "Paper", 2 = "Scissors".
+while(true) {
+    // "validchoice" checks if a valid answer was given or not. Otherwise, we alert the user that either an invalid answer or no answer was given and loop.
+    validchoice = false;
 
-// Display a message: "Rock, Paper, Scissors, Shoot:" and get user input of one of the three options
-// Convert to numbers as indicated next to the "play" variable
-// "validchoice" checks if a valid answer was given or not. Otherwise, we alert the user that either an invalid answer or no answer was given and loop.
-while(validchoice == false) {
-    playerNum = prompt("Rock, Paper, Scissors, Shoot!");
+    while(validchoice == false) {
+        // Display a message: "Rock, Paper, Scissors, Shoot:" and get user input of one of the three options
+        playerNum = prompt("Rock, Paper, Scissors, Shoot!");
 
-    if (playerNum == null || playerNum == "") {
-        alert("No answer given. Please try again.");
+        // Convert to numbers as indicated next to the "play" variable
+        if (playerNum == null || playerNum == "") {
+            alert("No answer given. Please try again.");
+        }
+
+        else if (playerNum.toLowerCase() == "rock") {
+            playerNum = 0;
+            validchoice = true;
+        }
+
+        else if (playerNum.toLowerCase() == "paper") {
+            playerNum = 1;
+            validchoice = true;
+        }
+
+        else if (playerNum.toLowerCase() == "scissors") {
+            playerNum = 2
+            validchoice = true;
+        }
+
+        else {
+            alert("Invalid answer. Please try again.")
+        }
     }
 
-    else if (playerNum.toLowerCase() == "rock") {
-        playerNum = 0;
-        validchoice = true;
-    }
+    // picks a random integer between 0-2. O = "Rock", 1 = "Paper", 2 = "Scissors".
+    let cpuNum = (Math.floor(Math.random() * 3));
 
-    else if (playerNum.toLowerCase() == "paper") {
-        playerNum = 1;
-        validchoice = true;
-    }
+    // possible outcomes after choices are made
+    switch (true) {
+        // equal numbers means a tie
+        case (playerNum == cpuNum):
+            alert(`You both played ${choices[playerNum]}.\nTie!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
+            break;
+        
+        // if playerNum is less than cpuNum, and cpuNum is not 3 (scissors), then cpu beats player
+        case (playerNum < cpuNum && cpuNum - playerNum == 1):
+            cpuScore += 1;
+            alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\n Computer wins!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
+            break;
 
-    else if (playerNum.toLowerCase() == "scissors") {
-        playerNum = 2
-        validchoice = true;
-    }
+        // if cpuNum is less than playerNum, and playerNum is not 3 (scissors), then player beats cpu
+        case (cpuNum < playerNum && playerNum - cpuNum == 1):
+            playerScore += 1
+            alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\n You win!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
+            break;
 
-    else {
-        alert("Invalid answer. Please try again.")
+        // if cpuNum is less than playerNum, and playerNum is 3 (scissors), then cpu beats player
+        // case strictly for rock vs scissors
+        case (cpuNum < playerNum && playerNum - cpuNum == 2):
+            cpuScore += 1;
+            alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\n Computer wins!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
+            break;
+        
+        // if playerNum is less than cpuNum, and cpuNum is 3 (scissors), then player beats cpu
+        // case strictly for rock vs scissors
+        case (playerNum < cpuNum && cpuNum - playerNum == 2):
+            playerScore += 1;
+            alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\n You win!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
     }
 }
-// if player number == computer number:
-//      return "Tie", no score increment
-// if player number < computer number and player number != 0:
-//      computer wins, increment score by 1.
-// else if player number > computer number and player number != 2:
-//      Player wins, increment score by 1
-// else if player number < computer number:
-//      computer wins, increment score by 1
-// else if player number > computer number:
-//      player wins, increment score by 1
-
-console.log(cpuNum);
-console.log(playerNum);
-
-switch (true) {
-    case (playerNum == cpuNum):
-        alert(`You both played ${choices[playerNum]}.\nTie!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
-        break;
-    case (playerNum < cpuNum && cpuNum - playerNum == 1):
-        alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\n Computer wins!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
-        cpuScore += 1;
-        break;
-    case (cpuNum < playerNum && playerNum - cpuNum == 1):
-        alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\n You win!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
-        playerScore += 1
-        break;
-    case (playerNum < cpuNum && cpuNum - playerNum == 2):
-        alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\n Computer wins!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
-        cpuScore += 1;
-        break;
-    case (cpuNum < playerNum && playerNum - cpuNum == 2):
-        alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\n You win!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
-        playerScore += 1;
-}
-
