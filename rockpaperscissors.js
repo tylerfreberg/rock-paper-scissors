6324a1d37605b0fd95d52fd69720345abf442424
Tyler Freberg <33191810+tylerfreberg@users.noreply.github.com>
2026-03-02 01:59:29 -0600

    Separated into functions. Made getComputerChoice, getHumanChoice, and playRound(). Added a check for restarting the game. Completed project.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,46 +4,43 @@
 // Computer prompts user for a choice: rock, paper, or scissors. Rock beats scissors, scissors beats paper, and paper beats rock.
 // If the user makes the same choice as the computer, it's a tie, and no points are given.
 
-// Scores. Best of 3.
-let playerScore = 0
-let cpuScore = 0
-let playerNum;
-let validchoice;
-let choices = ["rock", "paper", "scissors"];
+// returns a random integer between 0-2. O = "Rock", 1 = "Paper", 2 = "Scissors".
+function getComputerChoice() {
+    return(Math.floor(Math.random() * 3));
+}
 
-while (playerScore != 3 && cpuScore != 3) {
-    // "validchoice" checks if a valid answer was given or not. Otherwise, we alert the user that either an invalid answer or no answer was given and loop.
-    validchoice = false;
+// returns a number based on player input. O = "Rock", 1 = "Paper", 2 = "Scissors".
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper, Scissors, Shoot!");
 
-    while(validchoice == false) {
-        // Display a message: "Rock, Paper, Scissors, Shoot:" and get user input of one of the three options
-        playerNum = prompt("Rock, Paper, Scissors, Shoot!");
-
-        // Convert to numbers as indicated next to the "play" variable
-        if (playerNum == null || playerNum == "") {
+    // Convert to numbers as indicated next to the "play" variable
+    switch (true) {
+        case (choice == null || choice == ""):
             alert("No answer given. Please try again.");
-        }
-        else if (playerNum.toLowerCase() == "rock") {
-            playerNum = 0;
-            validchoice = true;
-        }
-        else if (playerNum.toLowerCase() == "paper") {
-            playerNum = 1;
-            validchoice = true;
-        }
-        else if (playerNum.toLowerCase() == "scissors") {
-            playerNum = 2
-            validchoice = true;
-        }
-        else {
-            alert("Invalid answer. Please try again.")
-        }
+            return(getHumanChoice())
+
+        case (choice.toLowerCase() == "rock"):
+            return(0);
+
+        case (choice.toLowerCase() == "paper"):
+            return(1);
+
+        case (choice.toLowerCase() == "scissors"):
+            return(2);
+
+        default:
+            alert("Invalid answer. Please try again.");
+            return(getHumanChoice())
     }
+}
 
-    // picks a random integer between 0-2. O = "Rock", 1 = "Paper", 2 = "Scissors".
-    let cpuNum = (Math.floor(Math.random() * 3));
+// plays a round of Rock, Paper, Scissors
+function playRound() {
+    let playerNum = getHumanChoice();
+    let cpuNum = getComputerChoice();
+    let choices = ["rock", "paper", "scissors"];
 
-    // possible outcomes after choices are made
+    // Using numbers for comparison instead of strings, as it amounts to less potential cases when you use a little math.
     switch (true) {
         // equal numbers means a tie
         case (playerNum == cpuNum):
@@ -75,6 +72,31 @@ while (playerScore != 3 && cpuScore != 3) {
             playerScore += 1;
             alert(`You played ${choices[playerNum]}.\nComputer played ${choices[cpuNum]}.\nYou win!\nYou: ${playerScore}\nCPU: ${cpuScore}`);
     }
+
+    if (playerScore == 3 || cpuScore == 3) {
+        let playAgain = prompt(`Game Over! ${playerScore > cpuScore ? "You win!" : "You lose!"}\nYou: ${playerScore}\nCPU: ${cpuScore}\nPlay again? (Y/N)`);
+        
+        if(playAgain.toLowerCase() == 'y') {
+            playerScore = 0;
+            cpuScore = 0;
+        }
+
+        else if(playAgain.toLowerCase() == 'n') {
+            alert("Thank you for playing! Goodbye.");
+        }
+
+        else {
+            alert("Unexpected input. Ending game. Goodbye.");
+        }
+    }
 }
 
-prompt(`Game Over! ${playerScore > cpuScore ? "You win!" : "You lose!"}\nYou: ${playerScore}\nCPU: ${cpuScore}`)
+let playerScore = 0
+let cpuScore = 0
+
+while (playerScore != 3 && cpuScore != 3) {
+    // "validchoice" checks if a valid answer was given or not. Otherwise, we alert the user that either an invalid answer or no answer was given and loop.
+    playRound();
+ 
+}
+
